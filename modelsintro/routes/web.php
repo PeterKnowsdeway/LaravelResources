e978e83e80828d424ce8e1a37000e8a2570b544f
PeterKnowsdeway <43824950+PeterKnowsdeway@users.noreply.github.com>
2020-05-06 15:38:24 -0500

    Added login stuff

--- modelsintro/routes/web.php
+++ modelsintro/routes/web.php
@@ -25,3 +25,7 @@ Route::get('/customers', "CustomersController@index");
 Auth::routes();
 
 Route::get('/home', 'HomeController@index')->name('home');
+
+Auth::routes();
+
+Route::get('/home', 'HomeController@index')->name('home');
